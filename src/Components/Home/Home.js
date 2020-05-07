4fd4c43d206e6a63606475a8209c379113de6d26
YunJu Lee <ollabu3@gmail.com>
2020-05-07 17:35:13 +0900

    add on home style

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <div></div>;
+  // <section>  </section>
+  // <div class="bg"></div>
+  // <h1>Learn to code by cloning real services</h1>
+  // <p>Over 6,000+ students are cloning Instagram, Bitcoin, Netflix, Uber, and More!</p>
+  // <a href="#" class="btn">Start Coding Like a Boss</a>
+  return (
+    <section>
+      <div className="background_Color"></div>
+      <p>Front-End Developer 이윤주 입니다</p>
+    </section>
+  );
 }
 
 export default Home;
