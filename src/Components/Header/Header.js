2041e6d3fe44966a50b6b2451b9083eeb64e2bb7
YunJu Lee <ollabu3@gmail.com>
2020-05-07 14:09:35 +0900

    add on header style

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
-  return <div></div>;
+  return (
+    <header className="header_Container">
+      <div className="header">
+        <h3 className="header_Title">YunJuLee</h3>
+        <ul className="header_Menu">
+          <li className="menu_List">About</li>
+          <li className="menu_List">Skills</li>
+          <li className="menu_List">Projects</li>
+          <li className="menu_List">Contact</li>
+          <li className="menu_List">Resume</li>
+        </ul>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
