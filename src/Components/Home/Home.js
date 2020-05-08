9b1c4dada19504a79c73a11c28a4353162928253
YunJu Lee <ollabu3@gmail.com>
2020-05-08 16:14:09 +0900

    fix Home title z-index

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,8 +4,8 @@ import "./Home.css";
 function Home() {
   return (
     <section className="Home_Section">
-      <div className="background_Color"></div>
-      <p className="Home_Titile">Front-End Developer 이윤주 입니다</p>
+      <div></div>
+      <p>Front-End Developer 이윤주 입니다</p>
     </section>
   );
 }
