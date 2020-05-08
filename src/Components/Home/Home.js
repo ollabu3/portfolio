af0cd0503e455af276eb2192c16cdec59459b142
YunJu Lee <ollabu3@gmail.com>
2020-05-08 15:45:50 +0900

    fixed Home title font size

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,15 +2,10 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  // <section>  </section>
-  // <div class="bg"></div>
-  // <h1>Learn to code by cloning real services</h1>
-  // <p>Over 6,000+ students are cloning Instagram, Bitcoin, Netflix, Uber, and More!</p>
-  // <a href="#" class="btn">Start Coding Like a Boss</a>
   return (
-    <section>
+    <section className="Home_Section">
       <div className="background_Color"></div>
-      <p>Front-End Developer 이윤주 입니다</p>
+      <p className="Home_Titile">Front-End Developer 이윤주 입니다</p>
     </section>
   );
 }
