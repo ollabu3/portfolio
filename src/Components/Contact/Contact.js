952dee09f6df609c9a455b169c8ccf7c5371341f
YunJu Lee <ollabu3@gmail.com>
2020-05-12 22:29:41 +0900

    add on Contact style

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,7 +1,36 @@
 import React from "react";
 import "./Contact.css";
+import { Container, Row, Col } from "react-grid-system";
 
 function Contact() {
-  return <div></div>;
+  return (
+    <section>
+      <div className="contact">
+        <div className="contact_Title">Contact</div>
+        <Container className="Contatner">
+          <Row>
+            <Col>
+              <div className="contact_name">Blog</div>
+              <a href="https://velog.io/@ollabu3">
+                <div className="contact_detail">velog.io/ollabu3</div>
+              </a>
+            </Col>
+            <Col>
+              <div className="contact_name">Github</div>
+              <a href="https://github.com/ollabu3">
+                <div className="contact_detail">github.com/ollabu3</div>
+              </a>
+            </Col>
+            <Col>
+              <div className="contact_name">Email</div>
+              <a href="mailto:ollabu3@gmail.com">
+                <div className="contact_detail">ollabu3@gmail.com</div>
+              </a>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </section>
+  );
 }
 export default Contact;
