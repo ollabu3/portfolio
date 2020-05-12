56e624eb170e2233df2069694c9f00978e671fc6
YunJu Lee <ollabu3@gmail.com>
2020-05-12 21:50:14 +0900

    add on Project style

--- src/Components/Projects/Components/ProjectBox.js
+++ src/Components/Projects/Components/ProjectBox.js
@@ -1,16 +1,62 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
+import "./ProjectBox.css";
+import { Container, Row, Col } from "react-grid-system";
 
 function ProjectBox({ list }) {
-  //   console.log(list);
   return (
-    <div>
-      <div>{list.name}</div>
-      <div>{list.position}</div>
-      <p>{list.term}</p>
-      <div>{list.detail}</div>
-      <div></div>
-    </div>
+    <Container>
+      <Row className="Box">
+        <Col md={4}>
+          {list.name === "MukBank(먹뱅크)" ? (
+            <img
+              className="projectImg"
+              src={require("./ProjectsImg/MukBank.png")}
+            />
+          ) : (
+            <img
+              className="projectImg"
+              src={require("./ProjectsImg/WordMon.png")}
+            />
+          )}
+        </Col>
+        <Col md={8}>
+          <div className="textDiv">
+            <div className="name">{list.name}</div>
+            <div className="position">position | {list.position}</div>
+            <p className="term">{list.term}</p>
+            <div className="detail">{list.detail}</div>
+
+            {list.name === "MukBank(먹뱅크)" ? (
+              <div>
+                <a>
+                  <img
+                    className="logoImg"
+                    src={require("./ProjectsImg/github_Logo.png")}
+                  />
+                </a>
+                <a href="https://www.notion.so/3-ToNaEat-MukBank-73a4f6f672fd456683659ab84dc02950">
+                  <img
+                    className="logoImg"
+                    src={require("./ProjectsImg/notion_Logo.png")}
+                  />
+                </a>
+              </div>
+            ) : (
+              <div>
+                <img
+                  className="logoImg"
+                  src={require("./ProjectsImg/github_Logo.png")}
+                />
+                <div></div>
+              </div>
+            )}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
 export default ProjectBox;
+//
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
 import "./Projects.css";
-import { Container, Row, Col } from "react-grid-system";
 import { ProjectsData } from "./Components/ProjectsData";
 import ProjectBox from "./Components/ProjectBox";
 
 function Projects() {
   const [projectsData] = useState(ProjectsData);
+
   return (
     <section className="projects_Section">
       <div className="projects">
         <div className="projects_Title">Projects</div>
       </div>
-      <Container>
-        {projectsData.map((list) => (
-          <div key={JSON.stringify(list.id)}>
-            <ProjectBox list={list} />
-          </div>
-        ))}
-      </Container>
+      {projectsData.map((list) => (
+        <div className="ProjectBox" key={JSON.stringify(list.id)}>
+          <ProjectBox list={list} />
+        </div>
+      ))}
     </section>
   );
 }
