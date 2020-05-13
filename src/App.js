a330e853906b91469c2fabead33b9354e01e01ca
YunJu Lee <ollabu3@gmail.com>
2020-05-13 16:21:41 +0900

    add on Footer Component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./Components/About/About";
 import Skills from "./Components/Skills/Skills";
 import Projects from "./Components/Projects/Projects";
 import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
