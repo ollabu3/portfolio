2e47c095d57e2b65eec68aed26b7bed941fc82d0
YunJu Lee <ollabu3@gmail.com>
2020-05-24 16:36:22 +0900

    Fix: add on skills style in SkillsData

--- src/Components/Skills/Components/SkillsData.js
+++ src/Components/Skills/Components/SkillsData.js
@@ -2,13 +2,18 @@ export const SkillsData = [
   {
     id: 1,
     position: "Frontend",
-    skills: "HTML·CSS·React·React-Native·React-Navigation·React-Hooks",
+    skills:
+      "HTML · CSS · React · React-Native · React-Navigation · React-Hooks",
   },
-  { id: 2, position: "Backend", skills: "Node.js·Express·MySQL·Sequelize" },
+  {
+    id: 2,
+    position: "Backend",
+    skills: "Node.js · Express · MySQL · Sequelize",
+  },
   {
     id: 3,
     position: "Development Tools",
-    skills: "Git·AWS·Visual Studio Code",
+    skills: `Git · AWS · Visual Studio Code`,
   },
 ];
 // 아이콘 제작자 <a href="https://www.flaticon.com/kr/authors/dinosoftlabs" title="DinosoftLabs">DinosoftLabs</a> from <a href="https://www.flaticon.com/kr/" title="Flaticon"> www.flaticon.com</a>
